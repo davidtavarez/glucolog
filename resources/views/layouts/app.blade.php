356786a94beb29deacb41684f59782a385cb9442
Nelson Romero <nromero125@gmail.com>
2018-03-08 10:36:42 -0400

    Traducción al Español #6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,8 +34,8 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                            <li><a class="nav-link" href="{{ route('login') }}">Entrar</a></li>
+                            <li><a class="nav-link" href="{{ route('register') }}">Registrarse</a></li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
