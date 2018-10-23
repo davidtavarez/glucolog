5a263d21df54d149d32e365eceac631c522ce793
Nelson Romero <nromero125@gmail.com>
2018-10-23 11:38:56 -0400

    refactoring controller and repos

--- app/Repositories/Eloquent/RecordRepository.php
+++ app/Repositories/Eloquent/RecordRepository.php
@@ -1,22 +1,113 @@
 <?php
 
 namespace App\Repositories\Eloquent;
-use App\Repositories\Contracts\RecordInterface;
+
 use App\Models\Record;
+use App\Repositories\Contracts\RecordInterface;
 use Auth;
+
 class RecordRepository implements RecordInterface
 {
+    public function index()
+    {
+        $records = Record::where('board_id', Auth::user()->board_id)->paginate(15);
+        return view('records.index', compact('records'));
+    }
+    
     public function store($request)
     {
         Record::create([
             'user_id' => Auth::user()->id,
-            'fecha' => $request->fecha,
-            'fecha' => $request->fecha,
-            'ayuno' => $request->ayuno,
-            'comentario' => $request->comentario,
-            'comida' => $request->comida,
-            'medida' => $request->medida,
-            'tipo_comida' => $request->tipo_comida
+            'board_id' => Auth::user()->board_id,
+            'date' => $request->date,
+            'status' => $this->validateCondition($request),
+            'comment' => $request->comment,
+            'food' => $request->food,
+            'measure' => $request->measure,
+            'condition' => $request->condition,
         ]);
+
+        return redirect('/records');
+    }
+
+    public function validateCondition($request)
+    {
+        $measure = $request->measure;
+        $condition = (int)$request->condition;
+        //Before eat
+        if($condition === 1) {
+            if($measure >= 65 && $measure <= 100){
+                return 'Ideal';
+            }
+
+            if($measure >= 70 && $measure <= 145){
+                return 'Buen control';
+            }
+
+            if($measure < 70 || ($measure > 145 && $measure <= 162)){
+                return 'Aceptable';
+            }
+
+            if($measure > 162){
+                return 'Mal control';
+            }
+        }
+
+        //After eat
+        if($condition === 2) {
+            if($measure >= 80 && $measure <= 126){
+                return 'Ideal';
+            }
+
+            if($measure >= 90 && $measure <= 180){
+                return 'Buen control';
+            }
+
+            if($measure < 70 || ($measure > 200 && $measure <= 250)){
+                return 'Aceptable';
+            }
+
+            if($measure > 250){
+                return 'Mal control';
+            }
+        }
+
+        //Before sleep
+        if($condition === 3) {
+            if($measure >= 80 && $measure <= 100){
+                return 'Ideal';
+            }
+
+            if($measure >= 120 && $measure <= 180){
+                return 'Buen control';
+            }
+
+            if(($measure > 80 && $measure <= 120) || ($measure > 180 && $measure <= 200)){
+                return 'Aceptable';
+            }
+
+            if($measure < 80 || $measure > 200){
+                return 'Mal control';
+            }
+        }
+
+        //Early Morning
+        if($condition === 4) {
+            if($measure >= 65 && $measure <= 100){
+                return 'Ideal';
+            }
+
+            if($measure >= 80 && $measure <= 162){
+                return 'Buen control';
+            }
+
+            if(($measure > 70 && $measure <= 80) || ($measure > 162 && $measure <= 200)){
+                return 'Aceptable';
+            }
+
+            if($measure < 70 || $measure > 200){
+                return 'Mal control';
+            }
+        }
     }
 }
