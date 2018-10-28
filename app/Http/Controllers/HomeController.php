e1fe5797122ee6b763f1a7423f5cf27a897b56f7
Clovis Ramírez <c.ramirez@gbh.com.do>
2018-10-28 16:33:37 -0400

    Use model namespace instead of absolute path

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Record;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $records = \App\Models\Record::all();
+        $records = Record::all();
         return view('home', compact('records'));
     }
 }
