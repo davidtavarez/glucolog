285bbeee95601b0b2d7cead3e6cbbfc3638c26ea
Nelson Romero <nromero125@gmail.com>
2018-11-04 16:26:14 -0400

    testing register method

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,15 +2,15 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-    use DatabaseTransactions, DatabaseMigrations;
+    use DatabaseTransactions, DatabaseMigrations, WithFaker;
 
     /**
      * @test
@@ -27,12 +27,14 @@ class AuthTest extends TestCase
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+
         $client = \DB::table('oauth_clients')->first();
         \DB::table('oauth_personal_access_clients')->insert([
             'client_id' => $client->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+
         $user = factory('App\Models\User')->create();
         $data = [
             'email' => $user->email,
@@ -54,8 +56,28 @@ class AuthTest extends TestCase
         $headers = [
             'Authorization' => 'Bearer ' . $token,
         ];
-        $this->post(route('auth.logout'),[], $headers)->assertStatus(200)->assertJson([
-            'message' => 'Te has desconectado correctamente.'
+        $this->post(route('auth.logout'), [], $headers)->assertStatus(200)->assertJson([
+            'message' => 'Te has desconectado correctamente.',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function user_can_register()
+    {
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'secret',
+            'birthday' => $this->faker->date,
+            'detection_date' => $this->faker->date,
+            'diabetes' => 'Type 1',
+            'sex' => 'Male',
+        ];
+
+        return $this->post(route('auth.register'), $data)->assertStatus(201)->assertJsonStructure([
+            'message', 'user',
         ]);
     }
 }
